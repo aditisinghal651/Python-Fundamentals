10e12782760482655747c024d598d5fecb3b369b
aditisinghal651 <aditisinghal121@gmail.com>
2025-05-20 14:59:43 +0530

    new changes

---
+++ 1-Python Basics/package/__init__.py
@@ -0,0 +1,11 @@
+'''
+__init__.py is a special Python file that indicates to the interpreter that the directory should be treated as a package.
+This file can be empty, or it can contain valid Python code. It is executed when the package is imported.
+The __init__.py file can also be used to define the public interface of the package by specifying which modules should be 
+imported when the package is imported.    
+
+This is a must have file for any directory that you want to be treated as a package.
+It can also be used to execute package initialization code.
+'''
+
+
---
+++ 1-Python Basics/package/test.py
@@ -0,0 +1,5 @@
+from package.maths import addition, subtraction
+
+print(addition(1, 2))
+
+print(subtraction(2, 1))
